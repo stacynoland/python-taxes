f1d5c74900718e9e0a648c19d33b534700e0b084
Stacy Noland <46572585+stacynoland@users.noreply.github.com>
2025-04-03 01:45:20 -0400

    Add zero and negative tests for income functions

---
+++ tests/federal/income_test.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+
+from pydantic import ValidationError
+import pytest
+
+from python_taxes.federal import income
+
+
+# Zero ($0) Tests
+@pytest.mark.parametrize(
+    "wages, expected",
+    [
+        (0, pytest.raises(ValidationError)),
+        ("0.00", pytest.raises(ValidationError)),
+        (Decimal("0.00"), pytest.raises(ValidationError)),
+    ],
+)
+class TestForZero:
+    def test_employer_withholding_zero(self, wages, expected):
+        with expected as e:
+            assert (income.employer_withholding (wages) == e)
+
+    def test_additional_withholding_zero(self, wages, expected):
+        with expected as e:
+            assert (income.employer_withholding_pre_2020(wages) == e)
+
+
+# Negative Tests
+@pytest.mark.parametrize(
+    "wages, expected",
+    [
+        (-1.00, pytest.raises(ValidationError)),
+        ("-10000", pytest.raises(ValidationError)),
+    ],
+)
+class TestForNegatives:
+    def test_required_withholding_negative(self, wages, expected):
+        with expected as e:
+            assert (income.employer_withholding(wages) == e)
+
+    def test_additional_withholding_negative(self, wages, expected):
+        with expected as e:
+            assert (income.employer_withholding_pre_2020(wages) == e)
